2da243dda0035329eed2b6148048969bfa18063a
Reyaad Rafeek <reyaad510@gmail.com>
2019-02-22 19:04:15 -0500

    Created class Dropdown

--- js/index.js
+++ js/index.js
@@ -6,30 +6,47 @@
 // Menu Overlay
 
 
-const menuButton = document.querySelector('.menu-button');
-const hamburgerContent = document.querySelector('.hamburger-content');
-const close = document.querySelector('.closebtn');
-let tween = TweenMax.to(".hamburger-content", 0.3, {xPercent:100, ease:Linear.easeInOut, paused:true});
+class Dropdown {
+    constructor(element) {
+        this.element = element;
+
+        this.menuButton = this.element.querySelector('.menu-button');
+
+        this.hamburgerContent = this.element.querySelector('.hamburger-content');
+
+        this.closeButton = this.element.querySelector('.closebtn');
+
+        this.tween = TweenMax.to(".hamburger-content", 0.3, {xPercent:100, ease:Linear.easeInOut, paused:true});
+
+        this.menuButton.addEventListener('click', () => {
+            this.open();
+        });
+
+        this.closeButton.addEventListener('click', () => {
+            this.close();
+        })
+    }
+
+    open() {
+
+        this.menuButton.classList.toggle('menu-hidden');
+        this.closeButton.classList.toggle('closebtn-hidden');
+        this.tween.play();
+    }
+
+    close() {
+
+        this.menuButton.classList.toggle('menu-hidden');
+        this.closeButton.classList.toggle('closebtn-hidden');
+        this.tween.reverse();
+
+    }
+}
 
 
-menuButton.addEventListener('click', e => {
-   
-    menuButton.classList.toggle('menu-hidden');
-    close.classList.toggle('closebtn-hidden');
-    tween.play();
-});
-
-close.addEventListener('click', e => {
-   
-    menuButton.classList.toggle('menu-hidden');
-    close.classList.toggle('closebtn-hidden');
-    tween.reverse();
-    
-})
 
 
-
-
+let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
 
 
 
