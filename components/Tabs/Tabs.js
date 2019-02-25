768222af9cff4f3158a155778a18ff397a0c00eb
Reyaad Rafeek <reyaad510@gmail.com>
2019-02-25 17:55:26 -0500

    Fixed bug when user clicked tab multiple times made content disappear

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -8,17 +8,25 @@ class TabLink {
         this.tabData = this.tab.dataset.tab;
         this.tabCard = document.querySelector(`.card[data-tab='${this.tabData}']`);
         this.tabCard = new TabCard(this.tabCard);
-        this.tab.addEventListener('click', this.tabClick.bind(this));
+        this.tab.addEventListener('click', () => {
+            this.tabClick(); 
+                
+        });
     }
 
     tabClick() {
+
         document.querySelectorAll('.link').forEach(link => link.classList.remove('active-tab'));
         
-
+        
         this.tab.classList.add('active-tab');
         this.tabCard.toggleCard();
+      
+        
         
     }
+
+
 }
 
 // Class TabCard
@@ -26,16 +34,19 @@ class TabLink {
 class TabCard {
     constructor(card) {
         this.card = card;
+       
     }
 
     toggleCard () {
 
-        document.querySelectorAll('.card').forEach(card => card.classList.remove('active-card'));
+       document.querySelectorAll('.card').forEach(card => card.classList.remove('active-card'));
 
        this.card.classList.add('active-card');
-         TweenMax.from(".card", 0.5, {opacity:0, scale:0, ease:AudioBufferSourceNode.easeOUt});
-
-       
+       if (!TweenMax.isTweening(this.card)) {
+        TweenMax.from(".card", 0.5, {opacity:0, scale:0});
+       } else {
+           console.log('Sorry I am tweening right now!');
+       } 
        
     }
 }
